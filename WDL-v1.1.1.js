816e36658953bc72a51700d3a8a0a2cdfe640b81
webpage-database-language <41171137+webpage-database-language@users.noreply.github.com>
2018-08-05 17:46:14 +1200

    Update WDL-v1.1.1.js

--- WDL-v1.1.1.js
+++ WDL-v1.1.1.js
@@ -8,7 +8,7 @@ while(item < SOURCEDDATABASES.length){
 	XMLHttpRequest.onreadystatechange = function(){
 		if(this.readyState == 4 && this.status == 200){
 			var newDatabase = document.createElement('database');
-			newDatabase.innerHTML = this.response;
+			newDatabase.innerHTML = this.responseText;
 			document.querySelector(SOURCEDDATABASES[item].attributes.location.value).appendChild(newDatabase);
 		};
 	};
