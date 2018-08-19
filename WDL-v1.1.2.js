1e0dacea843e04d3ea3257f3ea99f3f02d48043c
luisvallejomohl <38414346+luisvallejomohl@users.noreply.github.com>
2018-08-20 07:27:09 +1200

    Update WDL-v1.1.2.js

--- WDL-v1.1.2.js
+++ WDL-v1.1.2.js
@@ -20,6 +20,10 @@ for(var jtem = 0; jtem < databases.length; jtem++){
 		var afterKeyword = k[item + 1];
 		if(k[item] === '\nJS_EXECUTE'){
 			a += afterKeyword;
+		}else if(k[item] == '\nWDL_DEFFUNC'){
+			a += 'function ' + afterKeyword + '{';
+		}else if(k[item] == '\nWDL_RETURN'){
+			a += 'return ' + afterKeyword;
 		}else if(k[item] == '#'){
 			a += ' /*' + afterKeyword + '*/';
 		}else if(k[item] == '\nWDL_CREATETABLE'){
@@ -42,6 +46,8 @@ for(var jtem = 0; jtem < databases.length; jtem++){
 			}else{
 				console.error(afterKeyword.split('~')[0] + ' is defined. Use WDL_ALTER instead.');
 			};
+		}else if(k[item] == '\nWDL_SELECTFROM'){
+			a += 'var selectFromReturnValue = []; for(var item = 0; item < ' + afterKeyword.split('~')[0] + '.length; item++){if(' + afterKeyword.split('~')[2] + '){selectFromReturnValue.push(' + afterKeyword.split('~')[0] + '.' + afterKeywprd.split('~')[1]')}}'
 		}else if(k[item] == '\nWDL_ALTER'){
 			 a += afterKeyword.split('~')[0] + ' = ' + afterKeyword.split('~')[1] + ';';
 		}else if(k[item] == '\nWDL_SAVE'){
@@ -53,7 +59,7 @@ for(var jtem = 0; jtem < databases.length; jtem++){
 		}else if(k[item] == '\nWDL_GET'){
 			a += 'var ' + afterKeyword + ' = localStorage.getItem(\'' + afterKeyword + '\');' + afterKeyword + ' = JSON.parse(' + afterKeyword + ');';
 		}else if(k[item] == '\nWDL_PRINT'){
-			a += 'console.log(\'' + afterKeyword + '\');';
+			a += 'console.table(\'' + afterKeyword + '\');';
 		}else if(k[item] == '\nWDL_DELETE'){
 			a += 'if(protectedDatabases.indexOf(\'' + afterKeyword + '\') == -1){\
 				localStorage.removeItem(\'' + afterKeyword + '\');\
@@ -66,7 +72,7 @@ for(var jtem = 0; jtem < databases.length; jtem++){
 			};
 		}else if(k[item] == '\nWDL_IF'){
 			a += 'if(' + afterKeyword + '){';
-		}else if(k[item] == '\nWDL_ENDIF' || k[item] == '\nWDL_ENDLOOP'){
+		}else if(k[item] == '\nWDL_ENDIF' || k[item] == '\nWDL_ENDLOOP' || k[item] == '\nWDL_ENDFDEF'){
 			a += '};';
 		}else if(k[item] == '\nWDL_LOOP'){
 			a += 'for(var item = 0; item < ' + afterKeyword + '; item++){';
@@ -79,6 +85,74 @@ for(var jtem = 0; jtem < databases.length; jtem++){
 	databases[jtem].style.display = 'none';
 };
 const WDL_UNDEFINED = undefined;
+const WDL_PI = 3.1415926535897932384626433832795;
+const WDL_E = 2.718281828459045235386;
+const WDL_PHI = (1 + Math.sqrt(5))/2;
+const WDL_TRUE = true;
+const WDL_FALSE = false;
+const WDL_DATE = new Date();
+function WDL_ROOT(a, b){
+	return Math.pow(a, b);
+};
+function WDL_COS(a){
+	return Math.cos(a);
+};
+function WDL_SIN(a){
+	return Math.sin(a);
+};
+function WDL_TAN(a){
+	return Math.tan(a);
+};
+function WDL_ARCCOS(a){
+	return Math.acos(a);
+};
+function WDL_ARCSIN(a){
+	return Math.arcsin(a);
+};
+function WDL_ARCTAN(a){
+	return Math.arctan(a);
+};
+function WDL_STRINGIFY(a){
+	if(typeof a === undefined){
+		return 'WDL_UNDEFINED';
+	}else{
+		if(typeof a == 'string'){
+			return a;
+		}else if(typeof a == 'function'){
+			return a.toString();//.toString() is the best converter to strings for functions
+		}else if(a === true){
+			return 'WDL_TRUE'
+		}else if(a === false){
+			return 'WDL_FALSE';
+		}else{
+			return JSON.stringify(a); //Let JSON stringify a so that the return value is not '[object Object]'
+		};
+	};
+};
+function WDL_EVAL(data){
+	switch(data){
+		case 'WDL_UNDEFINED': return undefined; break; //Return undefined in the input is WDL_UNDEFINED
+		case 'WDL_TRUE': return true; break; //Return true if the input is WDL_TRUE
+		case 'WDL_FALSE': return false; break; //Return false if the input is WDL_FALSE
+		default: try{ return JSON.parse(data) } catch (e) { /*data must be javaScript*/ return eval(data)}; break;
+	};
+};
+function WDL_SELECTFROM(table, query, condition){
+	var myTable = [];
+	for(var item = 0; item < table.length; item++){
+		if(condition){
+			myTable.push(table[item][query]);
+		};
+	};
+	return myTable;
+};
+function WDL_JOIN(){
+    for(var item = 1; item < arguments.length; item++)
+        for(var key in arguments[item])
+            if(arguments[item].hasOwnProperty(key))
+                arguments[0][key] = arguments[item][key];
+    return arguments[0];
+}
 var WDLF = [];
 for(var item = 0; item < M.length; item++){
 	var _$_$ = new Function(M[item]);
